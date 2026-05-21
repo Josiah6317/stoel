6f0a7f22afaac11bc460b2c42f92106158f9d0f3
Josiah6317 <josiah.richardson@student.gildeopleidingen.nl>
2026-05-21 13:12:34 +0200

    Delete index (2) (1).php

--- index (2) (1).php
+++
@@ -1,8 +0,0 @@
-<?php
-  include "stoel.php";
-
-  $rodeStoel = new Stoel("rood", 60, 90);
-  echo $rodeStoel->echoZithoogte(); 
-  echo $rodeStoel->verstelZithoogte();
-  echo $rodeStoel ->echoKleur();
-?>
